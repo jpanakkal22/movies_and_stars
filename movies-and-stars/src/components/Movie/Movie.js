236cc0a43638fbd2d9cc74a044198a9595574bac
New Dad <josh_panakkal3122@hotmail.com>
2022-02-17 19:12:22 -0500

    map through starList array and render as links to actor page

--- movies-and-stars/src/components/Movie/Movie.js
+++ movies-and-stars/src/components/Movie/Movie.js
@@ -6,28 +6,38 @@ import { faArrowLeftLong } from '@fortawesome/free-solid-svg-icons';
 
 const Movie = () => {
     const [title, setTitle] = useState({});
+    const [isBusy, setBusy] = useState(true);
     let params = useParams();
 
     useEffect(() => {
         const fetchData = async() => {
             await axios.get('/api/Title/' + params.movieId)
-            .then(response => {
-              setTitle(response.data);
+            .then(response => { 
+                setBusy(false);          
+                setTitle(response.data);
             }) 
         }
         fetchData();           
-    }, []);
+    }, []);        
 
     return(
-        <div className="container fluid">           
+        <div className="container fluid">  
+        {console.log(title)}         
             <div className='row' id='#movieData'>
                 <div className='col-sm-4 d-flex justify-content-center'>
-                    <img class='movieImages' src={title.image} alt={title.title}/>
+                    <img className='movieImages' src={title.image} alt={title.title}/>
                 </div>
                 <div className='col-sm-8'>
                     <div className="row">
                         <h4>{title.title} ({title.year})</h4>
-                        <p>Starring: {title.stars}</p>
+                        <p>Starring: {title.starList? title.starList.map(actor => {
+                            return(
+                                <div><Link to={`/actorId/${actor.id}`}>{actor.name}</Link></div>
+                            )
+                        }): ''}</p>
+
+                        
+                       
                         <p>Rated: <strong>{title.contentRating}</strong></p>
                         <p>imDb Rating: {title.imDbRating}</p><hr/><br/>
                     </div>
